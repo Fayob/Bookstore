587ed4d08a3eb21674b9ee1349d61d8e750a995b
= <adedejifavour6@gmail.com>
2022-11-04 08:31:53 +0100

    Render all component in the App file

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import Navbar from './component/Navbar';
+
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="*" element={<h1>Error...</h1>} />
+    </Routes>
+  </Router>
+);
+
+export default App;
