9c8f46be8b0fffa2aff8d0843ed8e95ad9c19f2a
= <adedejifavour6@gmail.com>
2022-11-07 20:04:46 +0100

    Dispatch category action

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,25 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <button type="button">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories);
+
+  const status = () => {
+    dispatch(checkStatus());
+  };
+
+  useEffect(() => {
+    status();
+  }, []);
+
+  return (
+    <div>
+      {categories}
+    </div>
+  );
+};
 
 export default Categories;
