1488982f50664fea6dd7550f2dc877c1bf3089fb
= <adedejifavour6@gmail.com>
2022-11-10 13:03:14 +0100

    Add profile icon

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import profileLogo from '../assets/bookstore_profile_icon.jpg';
 
 const Navbar = () => (
-  <div>
-    <ul className="navbar_lists">
-      <li className="navbar_list">
-        <Link to="/"> Home </Link>
-      </li>
-      <li className="navbar_list">
-        <Link to="/categories"> Categories </Link>
-      </li>
-    </ul>
-  </div>
+  <nav className="navbar">
+    <div className="navbar_container">
+      <Link to="/"><h2>Bookstore CMS</h2></Link>
+      <ul className="navbar_lists">
+        <li className="navbar_list">
+          <Link to="/" className="book"> BOOK </Link>
+        </li>
+        <li className="navbar_list">
+          <Link to="/categories" className="categories"> CATEGORIES </Link>
+        </li>
+      </ul>
+    </div>
+    <div className="profile">
+      <img
+        src={profileLogo}
+        alt="profile_icon"
+        className="profile_img"
+      />
+    </div>
+  </nav>
 );
 
 export default Navbar;
